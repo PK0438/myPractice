f6fb6b0dd527923e361aeb4c7ef46e3ee50c0819
PK0438 <pk.1512963@gmail.com>
2025-06-28 01:07:52 -0400

    number guessing game

---
+++ practice16 - numberGuessingGame/ngg.js
@@ -0,0 +1,32 @@
+//Number Guessing Game
+
+const minNum = 1;
+const maxNum = 100;
+const answer = Math.floor(Math.random() *(maxNum - minNum +1) + minNum);
+
+console.log("Answer ->", answer);
+
+let attempts = 0;
+let guess;
+let running = true;
+
+while(running) {
+    guess = parseInt(prompt(`Guess a number between ${minNum} and ${maxNum}:`));
+
+if(isNaN(guess)) {
+     window.alert("Invalid input! Please enter a valid number!")
+    } else if( guess <minNum || guess > maxNum) {
+        window.alert(`Please enter a number between ${minNum} and ${maxNum}.`);
+    } else {
+        attempts++;
+        if (guess > answer) {
+            window.alert("Too high! Try again.");
+        } else if (guess < answer) {
+            window.alert("Too low! Try again.");
+        } else {
+            window.alert(`Congratulations! You guessed the number ${answer} in ${attempts} attempts.`);
+            running = false; // End the game when the correct guess is made
+        }
+    }
+    
+}
